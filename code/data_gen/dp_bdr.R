f9263be922cf7ac1c6d8f2ca4b5d35d4119dc6b2
echuu <chuu3ric@gmail.com>
2019-02-10 12:35:41 -0600

    reproduce both data gen examples in DP

--- code/data_gen/dp_bdr.R
+++ code/data_gen/dp_bdr.R
@@ -11,12 +11,76 @@
 #          X     : (N x 2) design matrix, covariates stored row-wise
 #          y     : (N x 1) vector of responses
 #          shape : (N x 2) shape parameters for each y_n
-dp = function(N, seed = 100) {
+r_f2 = function(N, X, seed = 100) {
     
-    # x_i = (1, x_{i2}), x_{i2} ~ unif(0, 1)
-    # f(y_i | x_i) = N(y_i; - 1 + 2x_{i2}, 0.01)
+    set.seed(seed)
+    
+    # mixture parameters
+    mu_1    = X[,2]
+    mu_2    = X[,2]^4
+    sigsq_1 = 0.01
+    sigsq_2 = 0.04
+    
+    p = exp(-2 * X[,2])        # probability of drawing from mixture 1
+    mix_indic = runif(N) >= p  # determine which density to draw from
+    
+    N_mix1 = sum(!mix_indic)   # number of draws from first mixture density
+    N_mix2 = sum(mix_indic)    # number of draws from second mixture density
+    
+    y = numeric(N)
+    y[!mix_indic] = rnorm(N_mix1, mean = mu_1[!mix_indic], sd = sqrt(sigsq_1))
+    y[mix_indic]  = rnorm(N_mix2, mean = mu_2[mix_indic],  sd = sqrt(sigsq_2))
     
     
+    return(list(y = y, mix = mix_indic))
+}
+
+# give conditional density of y | x
+d_rf2 = function(x, N = 1000) {
+    
+    mu_1    = x
+    mu_2    = x^4
+    sigsq_1 = 0.01
+    sigsq_2 = 0.04
+    
+    N_evals = length(y_grid)
+    
+    # p = exp(-2 * X)                  # probability of drawing from mixture 1
+    #mix_indic = runif(N_evals) >= p  # determine which density to draw from
+    
+    N_mix1 = sum(!mix_indic)         # num of draws from first mixture density
+    N_mix2 = sum(mix_indic)          # num of draws from second mixture density
+    
+    y = numeric(N)
+    y[!mix_indic] = rnorm(N_mix1, mean = mu_1, sd = sqrt(sigsq_1))
+    y[mix_indic]  = rnorm(N_mix2, mean = mu_2, sd = sqrt(sigsq_2))
+
+    
+    f_y = numeric(N_evals)
+    f_y[!mix_indic] = dnorm(y[!mix_indic], mean = mu_1, sd = sqrt(sigsq_1))
+    f_y[mix_indic]  = dnorm(y[mix_indic],  mean = mu_2, sd = sqrt(sigsq_2))
+    
+    return(data.frame(y, f_y))
+}
+
+
+d_rf2 = function(x, y_grid) {
+    
+    mu_1    = x
+    mu_2    = x^4
+    sigsq_1 = 0.01
+    sigsq_2 = 0.04
+    
+    N_evals = length(y_grid)
+    
+    p = exp(-2 * x)                  # probability of drawing from mixture 1
+    #mix_indic = runif(N_evals) >= p  # determine which density to draw from
+    
+    f_y = numeric(N_evals)
+    f_y = p * dnorm(y_grid, mean = mu_1, sd = sqrt(sigsq_1)) + 
+        (1 - p) * dnorm(y_grid, mean = mu_2, sd = sqrt(sigsq_2))
+    
+    return(data.frame(y = y_grid, f_y))
 }
 
 
@@ -25,9 +89,10 @@ library(ggplot2)
 set.seed(1)
 N = 500
 p = 2
+
+# first simutlation ------------------------------------------------------------
+
 sigma_sq = 0.01
-
-
 x_i1 = rep(1, N) # 1st column of X
 x_i2 = runif(N)  # 2nd column of X
 
@@ -51,6 +116,31 @@ f_yx = dnorm(y_grid, mean = -1 + 2 * x, sd = sqrt(sigma_sq))
 df_y = data.frame(x = y_grid, y = f_yx)
 ggplot(df_y, aes(x, y)) + geom_point(size = 0.9) +
     scale_x_continuous(breaks = seq(-1.5, 1.5, by = 0.5))
+
+df_yx = data.frame(x = X[,2], y = y)
+
+ggplot(df_yx, aes(x, y)) + geom_point()
+
+
+# second simulation ------------------------------------------------------------
+
+
+y_rf2 = r_f2(N, X)
+y2 = y_rf2$y
+y2_mix = y_rf2$mix
+
+# 6th sublot in Figure 3
+df_yx = data.frame(x = X[,2], y = y2)
+ggplot(df_yx, aes(x = y, y = fy)) + geom_point() + stat_smooth() + theme_bw()
+
+
+# 1st subplot
+y_grid = seq(-0.5, 1.5, length.out = 500)
+x = 0.75
+f_yx = d_rf2(x, y_grid)
+# df_y = data.frame(x = y_grid, y = f_yx)
+ggplot(f_yx, aes(x = y, y = f_y)) + geom_point(size = 0.9) 
+
 
 
 
